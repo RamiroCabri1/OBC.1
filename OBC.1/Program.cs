9d130cdb9a5d787ec8de20c8c1ae5bda6261b6f4
RamiroCabri <cabriramiro@gmail.com>
2023-05-22 15:20:31 -0300

    iniciando

---
+++ OBC.1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OBC._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1 ");
+            
+            Console.WriteLine("Ingrese nombre ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hello, World! "+ nombre);
+            Console.WriteLine("_____________ ");
+
+            Console.WriteLine("Ejercicio 2 ");
+            
+            string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+            Console.WriteLine("La hora actual es: "+ datetime);
+            
+
+        }
+    }
+}
